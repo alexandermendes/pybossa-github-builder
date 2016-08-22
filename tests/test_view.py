846de4f918c5e6e8ccef2e0454309d7081d55233
alexandermendes <alexanderhmendes@gmail.com>
2016-08-22 23:33:50 +0100

    Update tests

--- tests/test_view.py
+++ tests/test_view.py
@@ -9,14 +9,13 @@ from default import flask_app, with_context
 from helper import web
 from pybossa_github_builder.view import _populate_form
 from pybossa_github_builder.forms import GitHubProjectForm
+from factories import ProjectFactory, UserFactory
 
 
 class TestView(web.Helper):
 
     def setUp(self):
         super(TestView, self).setUp()
-        with self.flask_app.app_context():
-            self.create()
         self.contents = {
             'project.json': {'name': 'project.json', 'download_url': 'url'},
             'results.html': {'name': 'results.html', 'download_url': 'url'},
@@ -26,45 +25,31 @@ class TestView(web.Helper):
             'long_description.md': {'name': 'long_description.md',
                                     'download_url': 'url'},
         }
+        self.owner = UserFactory.create(email_addr='a@a.com')
+        self.owner.set_password('1234')
+        self.signin(email='a@a.com', password='1234')
+        self.project = ProjectFactory.create(short_name="github_project",
+                                             owner=self.owner)
 
     def github_login(self):
         with self.app.session_transaction() as sess:
             sess['github_token'] = 'fake_token'
         self.signin(email=self.root_addr, password=self.root_password)
 
-    def test_get_new_github_project(self):
+    @with_context
+    def test_sync_github_project(self):
         self.github_login()
-        res = self.app.get('/github/project/new')
+        url = '/github/sync/{0}'.format(self.project.short_name)
+        res = self.app.get(url)
         assert res.status_code == 200
 
     @with_context
     @patch('pybossa_github_builder.view.github.authorize')
-    def test_github_login_specifies_full_url_as_callback(self, mock_authorize):
+    def test_login_redirects_to_full_callback_url(self, mock_authorize):
         mock_authorize.return_value = Response(302)
         redirect_uri = url_for('github.oauth_authorized', _external=True)
         self.app.get('/github/login')
         mock_authorize.assert_called_with(redirect_uri=redirect_uri)
-
-    @with_context
-    @patch('pybossa_github_builder.view.redirect', wraps=redirect)
-    def test_new_redirects_to_import_with_valid_github_url(self, redirect):
-        self.github_login()
-        data = dict(github_url='https://github.com/me/repo')
-        encoded_data = urllib.urlencode(data)
-        url = '/github/project/import_repo?{0}'.format(encoded_data)
-        self.app.post('/github/project/new', data=data)
-        redirect.assert_called_with(url)
-
-    @patch('pybossa_github_builder.view.GitHubRepo')
-    @patch('pybossa_github_builder.view.json')
-    def test_valid_project_imported(self, mock_json, mock_gh_repo):
-        mock_gh_repo = MagicMock()
-        mock_json = MagicMock()
-        self.github_login()
-        args = urllib.urlencode(dict(github_url='https://github.com/me/repo'))
-        url = '/github/project/import_repo?{0}'.format(args)
-        res = self.app.get(url)
-        assert res.status_code == 200, res.data
 
     @with_context
     def test_populate_form(self):
